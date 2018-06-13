8a574e9da781f7dbeea8464fb4b85fc2947b432c
Eugene Rubio <gnrubio09.er@gmail.com>
2018-06-13 09:37:59 +0800

    lotcreate tables

--- _php/QualityManagement/LotCreate.php
+++ _php/QualityManagement/LotCreate.php
@@ -35,8 +35,7 @@ position: inline-block;
                   <input type="textarea" class="form-control form-control-sm" id="Barcode_text" placeholder="SCAN DANPLA SERIAL NUMBER">
                     <div class="input-group-append">
                       <button style="z-index:0" type="button" class="btn btn-outline-secondary py-1" id="AddBtn" onclick="AddBtnClick()">ADD</button>
-                      <button style="z-index:0" type="button" class="btn btn-outline-secondary py-1" id="LotCreateBtn" onclick="generateLot()">LOT CREATE</button></td>
-                      <button style="z-index:0" type="button" class="btn btn-outline-secondary py-1" id="LotCreate_ExportBtn" onclick="notWorking()">EXPORT</button></td>
+                      <button style="z-index:0" type="button" class="btn btn-outline-secondary py-1" id="LotCreateBtn" onclick="generateLot()">LOT CREATE</button>
                     </div>
                 </div>
                 <div class="p-2 input-group">
@@ -48,6 +47,9 @@ position: inline-block;
                     <div class="input-group-text" id="btnGroupAddon2">TO</div>
                   </div>
                     <input id="lotDate2" type="date" class="py-1 form-control" onchange="SearchLotCreate()">
+                  <div class="input-group-append"> 
+                    <button style="z-index:0" type="button" class="btn btn-outline-secondary py-1" id="LotCreate_ExportBtn" onclick="notWorking()">EXPORT</button>
+                  </div>
                 </div>
 
 
@@ -64,60 +66,64 @@ position: inline-block;
               </div>
     </div>
 <div class="row">
-  <div class="d-flex col-12 justify-content-between">
-      <table class="table-wrapper-LotCreate table-bordered table-sm table table-hover mt-3" id='LotCreationTable' >
-                <?php       
-                        include $_SERVER['DOCUMENT_ROOT']."/1_mes/_includes/connect.php";  
+  <div class="col-12">
+    <div class="d-flex justify-content-between">
+      <div id="LotCreationTable" class="p-2 input-group">
+        <table class="table-wrapper-LotCreate table-bordered table-sm table table-hover mt-3">
+          <?php
+              include $_SERVER['DOCUMENT_ROOT']."/1_mes/_includes/connect.php";  
 
-                        $sql = "SELECT * FROM qmd_danpla_tempstore ORDER BY TEMP_ID DESC";
-                        $result = $conn->query($sql);
+              $sql = "SELECT * FROM qmd_danpla_tempstore ORDER BY TEMP_ID DESC";
+              $result = $conn->query($sql);
 
-                        if ($result->num_rows > 0) 
-                        {
-                            echo "<thead>    
-                            
-                            <th>DANPLA SERIES</th>
-                            <th>JOBORDER NO</th>
-                            <th>ITEM CODE</th>
-                            <th>QUANTITY</th>
-                            <th>MACHINE CODE</th>
-                            </thead><tbody>";
-                            // output data of each row
-                            while($row = $result->fetch_assoc()) 
-                            {
-                            echo " <tbody>";
-                        
-                            echo "<td>" . $row['DANPLA_SERIAL'] . "</td>";
-                            echo "<td>" . $row['JO_NUM'] . "</td>";
-                            echo "<td>" . $row['ITEM_CODE'] . "</td>";
-                            echo "<td>" . $row['QUANTITY'] . "</td>";
-                            echo "<td>" . $row['MACHINE_CODE'] . "</td>";
-                            }
-                            echo "</tbody></table>";
-                        } 
-                        else {
-                            echo "<thead>    
-                            
-                            <th>DANPLA SERIES</th>
-                            <th>JOBORDER NO</th>
-                            <th>ITEM CODE</th>
-                            <th>QUANTITY</th>
-                            <th>MACHINE CODE</th>
-                            </thead>
-                            <tbody>
-                                <td colspan='5' style='text-align:center'><h4>INSERT DANPLA DETAILS</h4></td>
-                              </tbody>
-                            </table>";
-                        }
-                        $conn->close();
-                  ?>
-                </table>
+              if ($result->num_rows > 0) 
+              {
+                  echo "<thead>    
+                  
+                  <th>DANPLA SERIES</th>
+                  <th>JOBORDER NO</th>
+                  <th>ITEM CODE</th>
+                  <th>QUANTITY</th>
+                  <th>MACHINE CODE</th>
+                  </thead><tbody>";
+                  // output data of each row
+                  while($row = $result->fetch_assoc()) 
+                  {
+                  echo " <tbody>";
+              
+                  echo "<td>" . $row['DANPLA_SERIAL'] . "</td>";
+                  echo "<td>" . $row['JO_NUM'] . "</td>";
+                  echo "<td>" . $row['ITEM_CODE'] . "</td>";
+                  echo "<td>" . $row['QUANTITY'] . "</td>";
+                  echo "<td>" . $row['MACHINE_CODE'] . "</td>";
+                  }
+                  echo "</tbody></table>";
+              } 
+              else {
+                  echo "<thead>    
+                  
+                  <th>DANPLA SERIES</th>
+                  <th>JOBORDER NO</th>
+                  <th>ITEM CODE</th>
+                  <th>QUANTITY</th>
+                  <th>MACHINE CODE</th>
+                  </thead>
+                  <tbody>
+                      <td colspan='5' style='text-align:center'><h4>INSERT DANPLA DETAILS</h4></td>
+                    </tbody>
+                  </table>";
+              }
+              $conn->close();
+          ?>
+        </table>
+      </div>
 
     
 
-        <div id="createdLotTable">
+        <div id="createdLotTable" class="p-2 input-group">
           <?php include $_SERVER['DOCUMENT_ROOT']."/1_mes/_php/QualityManagement/table/createdLot_table.php"; ?>
           </div>
+    </div>
     </div>
   </div>
 </div>
