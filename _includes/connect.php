10dc4b9ca247d1166eb4dd74c894208a8d2056cc
Edmund O. Mati Jr <ejvaux_05126@yahoo.com>
2018-07-11 14:19:33 +0800

    Fixed bug in connect.php

--- _includes/connect.php
+++ _includes/connect.php
@@ -6,13 +6,10 @@ $servername = $_ENV['DB_HOST'];
 $username = $_ENV['DB_USERNAME'];     
 $password = $_ENV['DB_PASSWORD'];
 $dbname = $_ENV['DB_DATABASE'];
-
 // Create connection
 $conn = new mysqli($servername, $username, $password,$dbname);
-
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
 ?>
--- _query/mold_repair/moldcode_ddl.php
+++ _query/mold_repair/moldcode_ddl.php
@@ -1,21 +1,14 @@
-<?php
-            
-
+<?php        
     include $_SERVER['DOCUMENT_ROOT']."/1_mes/_includes/connect.php";  
-
     $mcode = $_POST['mc'];
-
     $sql = "SELECT TOOL_NUMBER, ITEM_NAME, 	ITEM_CODE, CUSTOMER_NAME FROM dmc_mold_list WHERE MOLD_CODE = '$mcode' ";
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
-
     if(isset($row)){
         echo json_encode($row,true); 
     }
     else{
         echo "none";
-    }
-    
+    }    
     $conn->close();
-
 ?>
