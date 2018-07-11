0cb9acca17d128b6692506697980b84bc27b7aeb
Edmund O. Mati Jr <ejvaux_05126@yahoo.com>
2018-07-11 13:12:31 +0800

    Updated connect.ph. env used

--- _includes/connect.php
+++ _includes/connect.php
@@ -1,9 +1,17 @@
 <?php
+require $_SERVER['DOCUMENT_ROOT']. '/1_mes/vendor/autoload.php';
+$dotenv = new Dotenv\Dotenv($_SERVER['DOCUMENT_ROOT'].'\1_mes');
+$dotenv->load();
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USERNAME'];     
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_DATABASE'];
 
-$servername = "localhost";
+
+/* $servername = "localhost";
 $username = "root";     
 $password = "";
-$dbname = "masterdatabase";
+$dbname = "masterdatabase"; */
 
 // Create connection
 $conn = new mysqli($servername, $username, $password,$dbname);
