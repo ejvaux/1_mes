dd2135dc1ca540316d685b1c090a7dfba3ab5e9b
Eugene Rubio <gnrubio09.er@gmail.com>
2018-09-07 16:51:58 +0800

    add defect mgmt

--- _php/QualityManagement/table/DefectTable.php
+++ _php/QualityManagement/table/DefectTable.php
@@ -1,19 +1,19 @@
 
 <div class="row" style="width: 102%;">
       <div class="col"> 
-<table class='table table-hover table-bordered table-sm nowrap text-center' id='Dtable'>
-<thead> 
-        <th style="width: 5%">NO</th>
-        <th style="width: 5%">JOB ORDER</th>
-        <th style="width: 10%">PROD DATE</th>
-        <th style="width: 15%">ITEM CODE</th>
-        <th style="width: 20%">ITEM NAME</th>
-        <th style="width: 20%">JUDGE BY</th>
-        <th style="width: 10%">JUDGE DATETIME</th>
-        <th style="width: 5%">DEFECT QTY</th>
-        <th style="width: 20%">DEFECT NAME</th>
-</thead>
-</table>
+            <table class='table table-hover table-bordered table-sm nowrap text-center' id='Dtable'>
+                  <thead> 
+                        <th style="width: 5%">NO</th>
+                        <th style="width: 5%">JOB ORDER</th>
+                        <th style="width: 10%">PROD DATE</th>
+                        <th style="width: 15%">ITEM CODE</th>
+                        <th style="width: 20%">ITEM NAME</th>
+                        <th style="width: 20%">JUDGE BY</th>
+                        <th style="width: 10%">JUDGE DATETIME</th>
+                        <th style="width: 5%">DEFECT QTY</th>
+                        <th style="width: 20%">DEFECT NAME</th>
+                  </thead>
+            </table>
+      </div>
+</div>
 <?php include $_SERVER['DOCUMENT_ROOT']."/1_mes/_php/QualityManagement/_modal/InsertDefectModal.php"; ?>
-</div>
-</div>
