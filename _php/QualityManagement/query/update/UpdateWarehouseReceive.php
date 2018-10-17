802184f6ee4210ed9757e7fa2e2a3af2dc92acd3
Eugene Rubio <gnrubio09.er@gmail.com>
2018-10-17 16:40:05 +0800

    received by and date plus user in function loadDoc

--- _php/QualityManagement/query/update/UpdateWarehouseReceive.php
+++ _php/QualityManagement/query/update/UpdateWarehouseReceive.php
@@ -12,9 +12,10 @@ $lot_num = $_POST['lot_num'];
 $item = $_POST['item_code'];
 $lot_creator = $_SESSION['text'];
 $warehouse = $_POST['warehouse'];
+$receive_dateTime =  Date('Y-m-d H:i:s');
 include $_SERVER['DOCUMENT_ROOT']."/1_mes/_includes/connect.php";
 
-    $sql = "UPDATE qmd_lot_create SET TO_WAREHOUSE = '$warehouse', WAREHOUSE_RECEIVE = 'RECEIVED' WHERE LOT_NUMBER ='$lot_num' AND ITEM_CODE = '$item'";
+    $sql = "UPDATE qmd_lot_create SET TO_WAREHOUSE = '$warehouse', WAREHOUSE_RECEIVE = 'RECEIVED',	RECEIVED_BY = '$lot_creator', RECEIVED_DATE = '$receive_dateTime' WHERE LOT_NUMBER ='$lot_num' AND ITEM_CODE = '$item'";
     if($conn->query($sql) === TRUE) {
         include $_SERVER['DOCUMENT_ROOT']."/1_mes/_includes/connect.php";
                 $sql1 = "DELETE FROM qmd_item_tempstore WHERE INSERT_USER = '$lot_creator'";
