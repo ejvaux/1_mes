b8a97077e2391d9c6ae3eedec3b9c6d758b254f0
saturnzxc <dismagic001@gmail.com>
2020-02-18 09:35:13 +0800

    Management Indices

--- _php/management indices/SMT/DIPL1.php
+++ _php/management indices/SMT/DIPL1.php
@@ -10,11 +10,11 @@ $tplan=0;
       </tr>
 
   <tr align = 'center'> <th width = '100px' style='position: absolute;
-    display: flex;  background: #fff; font-size:12px; height:20px;'>PROD PLAN</th><td style='  padding-left: 90px;'></td><?php
+    display: flex;  background: #fff; font-size:11px; height:20px;font-family: Tahoma;'>PROD PLAN</th><td style='  padding-left: 90px;'></td><?php
     for ($fromstart1; $fromstart1 <=$toend1 ; $fromstart1++) { 
     if($stmt = $conn1->query("SELECT COUNT(DATE_), SUM(PLAN_QTY) FROM mis_prod_plan_dl WHERE DATE_ >='$start1' AND DATE_ADD(DATE_, INTERVAL 1 DAY) <='$end1' and JOB_ORDER_NO like'8%' AND MACHINE_CODE LIKE 'DIPL1' ")){while ($plan = $stmt->fetch_row()){
     $tplan+=$plan[1];
- echo "<td style='font-size:12px; height:20px;'>".number_format(@$plan[1],0,'.',',')."</td>";
+ echo "<td style='font-size:11px; height:20px;border: solid 0.5px black;font-family: Tahoma; background-color:#FFE699;'>".number_format(@$plan[1],0,'.',',')."</td>";
     $job_array[]=$plan[1];
 //$php_data_array[] = $row;
 
@@ -22,7 +22,7 @@ $tplan=0;
 $end1=date('Y-m-d',strtotime("$end1 +1 days"));
 
   }}}
- echo "<td style='font-size:12px; height:20px;'><b>".number_format(@$tplan,0,'.',',')."<b></td></tr>";
+ echo "<td style='font-size:11px; height:20px;border: solid 0.5px black;font-family: Tahoma; background-color:#FFE699;'><b>".number_format(@$tplan,0,'.',',')."<b></td></tr>";
 
 
 
@@ -32,13 +32,13 @@ $start2=date('Y-m-d H:i:s',strtotime($_POST['from'].' 06:00:00'));
 $end2=date('Y-m-d H:i:s',strtotime($_POST['from'].'+1 days'.' 05:59:59' ));
   $tresult=0;
            echo "<tr align = 'center'> <th w2dth = '100px' style='position: absolute;
-    display: flex;  background: #fff; font-size:12px; height:20px;'>PROD RESULT</th><td style='  padding-left: 90px;'></td>";
+    display: flex;  background: #fff; font-size:11px; height:20px;font-family: Tahoma;'>PROD RESULT</th><td style='  padding-left: 90px;'></td>";
       for ($fromstart2; $fromstart2 <=$toend2 ; $fromstart2++) { 
    if($stmt = $conn2->query("SELECT SUM(type) FROM pcb WHERE created_at>='$start2' AND DATE_ADD(created_at, INTERVAL 0 DAY) <='$end2' and jo_number like '8%' 
      and type = '1' AND line_id='19'  ")){
    $i=0;
   while ($result = $stmt->fetch_row()){
- echo "<td style='font-size:12px; height:20px;'>".number_format($result[0],0,'.',',') ."</td>";
+ echo "<td style='font-size:11px; height:20px;border: solid 0.5px black;font-family: Tahoma;'>".number_format($result[0],0,'.',',') ."</td>";
  $tresult+=$result[0];
  $result_array[] = $result[0];
  $php_data_array[] =$result[0];   
@@ -46,7 +46,7 @@ $end2=date('Y-m-d H:i:s',strtotime($_POST['from'].'+1 days'.' 05:59:59' ));
 $start2=date('Y-m-d H:i:s',strtotime("$start2 +1 days"));
 $end2=date('Y-m-d H:i:s',strtotime("$end2 +1 days"));
 }}}
-echo "<td style='font-size:12px; height:20px;'><b>".number_format($tresult,0,'.',',')."<b></td></tr>"; 
+echo "<td style='font-size:11px; height:20px;border: solid 0.5px black;font-family: Tahoma;'><b>".number_format($tresult,0,'.',',')."<b></td></tr>"; 
 
 
 
@@ -70,12 +70,12 @@ $start4=date('Y-m-d H:i:s',strtotime($_POST['from'].' 06:00:00'));
 $end4=date('Y-m-d H:i:s',strtotime($_POST['from'].'+1 days'.' 05:59:59' ));
  $tdef=0;
   echo "<tr align = 'center'> <th width = '100px' style='position: absolute;
-    display: flex;  background: #fff; font-size:12px; height:20px;'>DEFECT</th><td style='  padding-left: 90px;'></td>";
+    display: flex;  background: #fff; font-size:11px; height:20px;font-family: Tahoma;'>DEFECT</th><td style='  padding-left: 90px;'></td>";
        for ($fromstart4; $fromstart4 <=$toend4 ; $fromstart4++) { 
 if($stmt = $conn2->query("SELECT COUNT(line_id) FROM defect_mats WHERE  created_at>='$start4' AND DATE_ADD(created_at, INTERVAL 0 DAY) <='$end4'   AND line_id='19' ")){
 while ($def = $stmt->fetch_row()){
           $defect_array[]=@$def[0];
- echo "<td style='font-size:12px; height:20px;'>".number_format(@$def[0],0,'.',',')."</td>";
+ echo "<td style='font-size:11px; height:20px;border: solid 0.5px black;font-family: Tahoma;'>".number_format(@$def[0],0,'.',',')."</td>";
  $tdef+=@$def[0];
 $start4=date('Y-m-d H:i:s',strtotime("$start4 +1 days"));
 $end4=date('Y-m-d H:i:s',strtotime("$end4 +1 days"));
@@ -86,5 +86,5 @@ $end4=date('Y-m-d H:i:s',strtotime("$end4 +1 days"));
 
 
 
-echo "<td style='font-size:12px; height:20px;'><b>".number_format($tdef,0,'.',',')."<b></td></tr>";
+echo "<td style='font-size:11px; height:20px;border: solid 0.5px black;font-family: Tahoma;'><b>".number_format($tdef,0,'.',',')."<b></td></tr>";
 ?>
--- _php/management indices/SMT/SMTL2.php
+++ _php/management indices/SMT/SMTL2.php
@@ -13,11 +13,11 @@ $tplan=0;
       </tr>
 
   <tr align = 'center'> <th width = '100px' style='position: absolute;
-    display: flex;  background: #fff; font-size:12px; height:20px;'>PROD PLAN</th><td style='  padding-left: 90px;'></td><?php
+    display: flex;  background: #fff; font-size:11px; height:20px;font-family: Tahoma;'>PROD PLAN</th><td style='  padding-left: 90px;'></td><?php
     for ($fromstart1; $fromstart1 <=$toend1 ; $fromstart1++) { 
     if($stmt = $conn1->query("SELECT COUNT(DATE_), SUM(PLAN_QTY) FROM mis_prod_plan_dl WHERE DATE_ >='$start1' AND DATE_ADD(DATE_, INTERVAL 1 DAY) <='$end1' and JOB_ORDER_NO like'2%' AND MACHINE_CODE LIKE 'SMTL2'   ")){while ($plan = $stmt->fetch_row()){
     $tplan+=$plan[1];
- echo "<td style='font-size:12px; height:20px;'>".number_format(@$plan[1],0,'.',',')."</td>";
+ echo "<td style='font-size:11px; height:20px;border: solid 0.5px black;font-family: Tahoma; background-color:#FFE699;'>".number_format(@$plan[1],0,'.',',')."</td>";
     $job_array[]=$plan[1];
 //$php_data_array[] = $row;
 
@@ -25,7 +25,7 @@ $tplan=0;
 $end1=date('Y-m-d',strtotime("$end1 +1 days"));
 
   }}}
- echo "<td style='font-size:12px; height:20px;'><b>".number_format(@$tplan,0,'.',',')."<b></td></tr>";
+ echo "<td style='font-size:11px; height:20px;border: solid 0.5px black;font-family: Tahoma;background-color:#FFE699;'><b>".number_format(@$tplan,0,'.',',')."<b></td></tr>";
 
 
 
@@ -35,17 +35,17 @@ $start2=date('Y-m-d H:i:s',strtotime($_POST['from'].' 06:00:00'));
 $end2=date('Y-m-d H:i:s',strtotime($_POST['from'].'+1 days'.' 05:59:59' ));
   $tresult=0;
            echo "<tr align = 'center'> <th w2dth = '100px' style='position: absolute;
-    display: flex;  background: #fff; font-size:12px; height:20px;'>PROD RESULT</th><td style='  padding-left: 90px;'></td>";
+    display: flex;  background: #fff; font-size:11px; height:20px;font-family: Tahoma;'>PROD RESULT</th><td style='  padding-left: 90px;'></td>";
       for ($fromstart2; $fromstart2 <=$toend2 ; $fromstart2++) { 
    if($stmt = $conn2->query("SELECT sum(type) FROM pcb WHERE created_at>='$start2' AND DATE_ADD(created_at, INTERVAL 0 DAY) <='$end2' and jo_number like '2%' 
      and type = '1' AND line_id='2' ")){
   while ($result = $stmt->fetch_row()){
- echo "<td style='font-size:12px; height:20px;'>".number_format($result[0],0,'.',',') ."</td>";
+ echo "<td style='font-size:11px; height:20px;border: solid 0.5px black;font-family: Tahoma;'>".number_format($result[0],0,'.',',') ."</td>";
  $tresult+=$result[0];
 $start2=date('Y-m-d H:i:s',strtotime("$start2 +1 days"));
 $end2=date('Y-m-d H:i:s',strtotime("$end2 +1 days"));
 }}}
-echo "<td style='font-size:12px; height:20px;'><b>".number_format($tresult,0,'.',',')."<b></td></tr>"; 
+echo "<td style='font-size:11px; height:20px;border: solid 0.5px black;font-family: Tahoma;'><b>".number_format($tresult,0,'.',',')."<b></td></tr>"; 
 
 
 
@@ -58,12 +58,12 @@ $start4=date('Y-m-d H:i:s',strtotime($_POST['from'].' 06:00:00'));
 $end4=date('Y-m-d H:i:s',strtotime($_POST['from'].'+1 days'.' 05:59:59' ));
  $tdef=0;
   echo "<tr align = 'center'> <th width = '100px' style='position: absolute;
-    display: flex;  background: #fff; font-size:12px; height:20px;'>DEFECT</th><td style='  padding-left: 90px;'></td>";
+    display: flex;  background: #fff; font-size:11px; height:20px;font-family: Tahoma;'>DEFECT</th><td style='  padding-left: 90px;'></td>";
        for ($fromstart4; $fromstart4 <=$toend4 ; $fromstart4++) { 
 if($stmt = $conn2->query("SELECT SUM(division_id) FROM defect_mats WHERE division_id='2' AND created_at>='$start4' AND DATE_ADD(created_at, INTERVAL 0 DAY) <='$end4'   AND line_id='2' ")){
 while ($def = $stmt->fetch_row()){
   $defect=$def[0]/2;
- echo "<td style='font-size:12px; height:20px;'>".number_format($defect,0,'.',',')."</td>";
+ echo "<td style='font-size:11px; height:20px;border: solid 0.5px black;font-family: Tahoma;'>".number_format($defect,0,'.',',')."</td>";
  $tdef+=$defect;
 $start4=date('Y-m-d H:i:s',strtotime("$start4 +1 days"));
 $end4=date('Y-m-d H:i:s',strtotime("$end4 +1 days"));
@@ -74,6 +74,6 @@ $end4=date('Y-m-d H:i:s',strtotime("$end4 +1 days"));
 
 
 
-echo "<td style='font-size:12px; height:20px;'><b>".number_format($tdef,0,'.',',')."<b></td></tr>";
+echo "<td style='font-size:11px; height:20px;border: solid 0.5px black;font-family: Tahoma;'><b>".number_format($tdef,0,'.',',')."<b></td></tr>";
 ?>
 
